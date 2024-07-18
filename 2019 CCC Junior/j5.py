4cd49fc40a8f8289101b7e96e3fcc05464811c71
YarranC <yarrancui@gmail.com>
2024-07-18 11:24:21 -0700

    added a memo to improve the timing of 2019 j5

--- 2019 CCC Junior/j5.py
+++ 2019 CCC Junior/j5.py
@@ -8,36 +8,38 @@ for i in range(3):
 S, I, F = input().split()
 S = int(S)
 
-W = [] # rule, start, result
+W = []  # rule, start, result
+memo = {}
 
-def check(string, r, step):
-    if step == S and string == F:
-        return 1
-    elif step == S and string != F:
-        return 0
-    else:
-        rule = rules[r]
-        try: string.index(rule[0])
-        except: return 0
-        # replace old substring with new substring
-        i = string.index(rule[0])
-        while i < len(string):
-            if i != 0:
-                result = string[0:i]+rule[1]+string[i+len(rule[0]):]
-            else:
-                result = rule[1]+string[i+len(rule[0]):]
-            W.append([r+1, i+1, result])
-            for n in range(3):
-                if check(result, n, step+1) == 1:
-                   return 1
-            W.pop(len(W)-1)
-            try: string.index(rule[0], i+1)
-            except: return 0
-            i = string.index(rule[0], i+1)
-        
-for r in range(3):
-    if check(I, r, 0) == 1:
-        break
+def check(string, step):
+    if (string, step) in memo:
+        return memo[(string, step)]
+    
+    if step == S:
+        result = 1 if string == F else 0
+        memo[(string, step)] = result
+        return result
+    
+    for r, rule in enumerate(rules):
+        start = 0
+        while True:
+            i = string.find(rule[0], start)
+            if i == -1:
+                break
+            
+            result = string[:i] + rule[1] + string[i + len(rule[0]):]
+            W.append([r + 1, i + 1, result])
+            
+            if check(result, step + 1) == 1:
+                memo[(string, step)] = 1
+                return 1
+            
+            W.pop()
+            start = i + 1
+    
+    memo[(string, step)] = 0
+    return 0
 
-for w in W:
-    print(*w, sep=" ")
+if check(I, 0) == 1:
+    for w in W:
+        print(*w, sep=" ")
