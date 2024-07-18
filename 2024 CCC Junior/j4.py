a3499d59c271be661142e035ef3f2d44b27f8e4f
YarranC <135737564+YarranC@users.noreply.github.com>
2024-07-18 09:57:51 -0700

    Update j4.py
    improved 2024 J4

--- 2024 CCC Junior/j4.py
+++ 2024 CCC Junior/j4.py
@@ -1,41 +1,29 @@
 # Troublesome Keys
 
-def main():
-    right_key = '-'
-    silly_key = '-'
-    quiet_key = '-'
+right_key = '-'
+silly_key = '-'
+quiet_key = '-'
 
-    correct_string = input()
-    wrong_string = input()
+correct_string = input()+'*'
+wrong_string = input()+'*'
 
-    j = 0 # current correct string character position
+keys = [chr(i) for i in range(97, 123)]
+for k in keys:
+    if k in wrong_string and not k in correct_string:
+        silly_key = k
+        
+j = 0 
     
-    for i, cur_key in enumerate(correct_string):
-        # if quiet key, doesn't need to check again
-        if correct_string[i] != quiet_key: 
-            # if silly key, doesn't need to check again
-            if correct_string[i] == silly_key: 
-                j+=1
-            # if repeat key, go to the next one
-            elif i+1 < len(correct_string) and correct_string[i] == correct_string[i+1]: 
-                j+=1
-            # if found the wrong key
-            elif j < len(wrong_string) and cur_key != wrong_string[j]: 
-                # check if next character is the same, if same, found the quiet key, if not, found the silly key
-                if i+1 < len(correct_string) and correct_string[i+1] == wrong_string[j]:
-                    quiet_key = correct_string[i]         
-                else:
-                    right_key = cur_key
-                    silly_key = wrong_string[j]
-                    j+=1
-            # if the last character is the quiet key
-            elif j == len(wrong_string): 
-                quiet_key = correct_string[i]
-            else:
-                j+=1
-    
-    print(right_key, silly_key)
-    print(quiet_key)
+for i in range(len(correct_string)):
+    cur_key = correct_string[i]
+    if cur_key == wrong_string[j]:
+        j+=1
+    elif wrong_string[j] == silly_key:
+        right_key = cur_key
+        j+=1
+    else:
+        quiet_key = cur_key
 
-if __name__ == "__main__":
-    main()
+print(right_key, silly_key)
+print(quiet_key)
+
