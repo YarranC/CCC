6226af16558ce13025734979ba81a7297d43528a
YarranC <yarrancui@gmail.com>
2024-08-14 11:41:19 -0700

    updated 1996 CCC Senior S2: Divisibility by 11. now passed all three DMOJ test cases.

--- 1996 CCC Senior/s2.cpp
+++ 1996 CCC Senior/s2.cpp
@@ -2,108 +2,70 @@
 #include <string>
 using namespace std;
 
-bool check(string numStr, bool change) {
-    int len = numStr.length();
-
-    if (len < 19) {
-        long long num = stol(numStr);
-        if (num > 9 and !change) {
-            std::cout << num << std::endl;
-        }
-        else {
-            change = false;
-        }
-        if (num == 11) {
-            return true;
-        }
-        else if (num > 11) {
-            long long ud = num % 10;
-            long long t = num / 10;
-            return check(to_string(t - ud), false);
-        }
-        else {
-            return false;
-        }
-    }
-    else {
-        cout << numStr << endl;
-
-        while (numStr.length() > 18) {
-            long long num;
-            int l;
-            num = stol(numStr.substr(len - 18, 18));
-            numStr = numStr.substr(0, len - 18);
-
-            l = to_string(num).length();
-            if (l < 18) {
-                if (l <= 2) {
-                    num += 1000000000000000000;
-                    for (int i = numStr.length() - 1; i >= 0; i--) {
-                        if (numStr[i] == '0') {
-                            numStr[i] = '9';
-                        }
-                        else {
-                            if (i == 0 and int(numStr[i]) == 49) {
-                                numStr = numStr.substr(1, numStr.length() - 1);
-                                break;
-                            }
-                            else
-                            {
-                                numStr[i] = char(int(numStr[i]) - 1);
-                                break;
-                            }
-                        }
-                    }
-                }
-                else {
-                    for (int i = 0; i < 18 - l; i++) {
-                        numStr += '0';
-                    }
-                }
-            }
-
-            while (num > 9) {
-                long long ud = num % 10;
-                long long t = num / 10;
-                if (t - ud < 0) {
-                    break;
-                }
-                cout << numStr << t - ud << endl;
-
-                num = t - ud;
-            }
-
-            if (num != 0) {
-                numStr = numStr + to_string(num);
-                change = true;
-            }
-            else {
-                change = false;
-            }
-
-            len = numStr.length();
-        }
-
-        return check(numStr, change);
-    }
-}
-
 int main() {
-    int cnt;
-    std::cin >> cnt;
-    for (int i = 0; i < cnt; ++i) {
-        string num;
-        std::cin >> num;
+	int cnt;
+	cin >> cnt;
+	for (int i = 0; i < cnt; ++i) {
+		string num, orig_num;
+		int l;
+		cin >> num;
+		orig_num = num;
 
-        if (check(num, false)) {
-            std::cout << "The number " << num << " is divisible by 11." << std::endl;
-        }
-        else {
-            std::cout << "The number " << num << " is not divisible by 11." << std::endl;
-        }
-        if (i < cnt - 1) {
-            std::cout << std::endl;
-        }
-    }
-    return 0;
+		while (num.length() > 2) {
+			int idx, d, t;
+			l = num.length();
+			cout << num << endl;
+			idx = l - 1;
+			d = num[idx];
+			idx--;
+			t = num[idx];
+			if (d > t) {
+				t += 10;
+				if (num[idx - 1] != '0') {
+					num[idx - 1] = char(int(num[idx - 1]) - 1);
+				}
+				else {
+					num[idx - 1] = '9';
+					for (int j = idx - 2; j >= 0; j--) {
+						if (num[j] == '0') {
+							num[j] = '9';
+						}
+						else {
+							num[j] = char(int(num[j]) - 1);
+							break;
+						}
+					}
+				}
+			}
+			t -= d;
+
+			num[idx] = char(t + 48);
+
+			num = num.substr(0, l - 1);
+			if (num[0] == '0') {
+				num = num.substr(1, l - 1);
+			}
+		}
+
+		if (num.length() < 2) {
+			if (l > 2) {
+				cout << num << endl;
+			}
+			cout << "The number " << orig_num << " is not divisible by 11." << endl;
+		}
+		else if (num[0] == num[1]) {
+			cout << num << endl;
+			cout << "The number " << orig_num << " is divisible by 11." << endl;
+		}
+		else {
+			if (num.length() == 2) {
+				cout << num << endl;
+			}
+			cout << "The number " << orig_num << " is not divisible by 11." << endl;
+		}
+		if (i < cnt - 1) {
+			cout << endl;
+		}
+	}
+	return 0;
 }
